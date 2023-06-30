d51f33c951e4a128a876f6eb6f4ca58e260e5ab0
NicoBombastik <80768621+NicoJGutierrez@users.noreply.github.com>
2023-06-29 23:15:46 -0400

    feat: apuntes de R :ok_hand:

---
+++ parte1.R
@@ -0,0 +1,179 @@
+# Considere X~Poisson(7) y Y|X~Binomial(x,0.3)
+
+# Siendo que x determina la cantidad de intentos que habrán en la binomial,
+# se podría calcular la probabilidad merginal de y como una Poisson con
+# el intervalo modificado por la probabilidad de la binomial:
+7*(0.3)
+# = 2.1
+# Con eso tenemos que y marginal: Y~Poisson(2.1)
+
+# Calcule P(X = 7, Y = 4)
+# Tenemos que X e Y no son independientes eso si.
+dpois(7,7) * dbinom(4,7,0.3)
+
+# Calcule P(Y ≤ 4|X = 9)
+pbinom(4,9,0.3)
+
+# Calcule P(X = 10|Y = 7)
+# Cómo diablos calculo x dado y?
+# P(x|y) = P(y|x)*P(x)/P(y)
+dbinom(7,10,0.3) * dpois(10,7) / dpois(7,2.1)
+
+# Calcule P(X ≤ 10|Y = 7)
+dbinom(7,10,0.3) * dpois(10,7) / dpois(7,2.1)
+# Acabo de cachar que acá hay que usar un for
+X <- 0:10
+sum(dbinom(7,X,0.3) * dpois(X,7)) / dpois(7,2.1)
+
+# Considerando el modelo conjunto
+# X ∼ Binomial(12, 0.7) , Y |X = x ∼ Binomial(x, 0.3)
+# Calcule:
+
+# P(Y ≥ 5)
+# Para esto tenemos que sacar la marginal de Y:
+0.7 * 0.3
+# =0.21
+# Siendo que acá queremos mayor que le hacemos 1-p
+1 - pbinom(5, 12, 0.21)
+
+# P(3 ≤ Y ≤ 7|X = 6)
+pbinom(7, 6, 0.3) - pbinom(3, 6, 0.3)
+# Ojo porque es discreto, así que hay que usar 2 en lugar de 3:
+pbinom(7, 6, 0.3) - pbinom(2, 6, 0.3)
+
+# P(X = 6|Y = 3)
+# P(x|y) = P(y|x)*P(x)/P(y)
+dbinom(3, 6, 0.3) * dbinom(6, 12, 0.7) / dbinom(3, 12, 0.21)
+# Ojo que ese P(y|x) usa el y pedido para sacar probabilidades y que
+# el P(y) marginal es igual al anterior.
+
+# Considere:
+# (X,Y) ∼ NormalBivariada(µX,µY,σX,σY,ρ)
+# con µX = µY = 2, σX = 1, σY = 2, y ρ = 0.3. 
+# Calcule:
+# P(1 < X < 2; 1 < Y < 3)
+
+# Esto requiere mvtnorm:
+library(mvtnorm)
+medias <- c(2, 2)
+matrizCov <- matrix(c(1**2, rep(0.3, 1, 2, 2), 2**2),2,2)
+
+pmvnorm(lower = c(1,1), upper = c(2,3),
+        mean = medias, sigma = matrizCov)[1]
+
+#  P(X < 2.3)
+pmvnorm(lower = c(-Inf,-Inf), upper = c(2.3,Inf),
+        mean = medias, sigma = matrizCov)[1]
+
+# P(X > 1; Y < 2.3)
+pmvnorm(lower = c(1,-Inf), upper = c(Inf,2.3),
+        mean = medias, sigma = matrizCov)[1]
+
+# X1, . . . , X60 iid∼ Poisson(4)
+# calcular aproximadamente P(prom(X) ≤ 4.2). (Recordar corrección por continuidad)
+
+# Esto quiere decir, si tengo 60 Xs que distribuyen poisson con media 4
+# qué probabilidad tengo de que prom(x) < 4.2 ?
+
+# Siendo que tengo muchos datos, prom(x) = normal(mu, sigma/sqrt(n))
+pnorm(4.2, 4, sqrt(4)/sqrt(60))
+# Tengo que hacer un ajuste por continuidad, que básicamente es sumarle o
+# restarle 0,5 a todos los datos por separado:
+pnorm(4.2 + 0.5/60, 4, sqrt(4)/sqrt(60))
+# En este caso se suma porque (?), debería estar bien también la resta, quiza.
+
+# Si Y1, . . . , Y45 iid∼ Normal(4, 1), calcular P(3 < prom(Y) < 4.2).
+# Acá no se tiene que hacer ajuste por continuidad:
+pnorm(4.2, 4, 1/sqrt(45)) - pnorm(3, 4, sqrt(1)/sqrt(45))
+
+# Si Z1, . . . , Z40 iid∼ Gamma(2, 2), calcular aproximadamente P(prom(Z) > 1.1).
+1 - pnorm(1.1, 1, sqrt(1/2)/sqrt(40))
+
+# Si W1, . . . , W120 iid∼ Bernoulli(0.2), calcular aproximadamente 
+# P(prom(W) ≥ 0.25). (Recordar corrección por continuidad)
+1 - pnorm(0.25, 0.2, sqrt(0.2*(1-0.2))/sqrt(120))
+# Ajustado por continuidad:
+1 - pnorm(0.25-0.5/120, 0.2, sqrt(0.2*(1-0.2))/sqrt(120))
+# Se resta porque estamos restando en un principio, y p*(1-p) es la varianza
+# de la bernoulli
+
+# Si X1, . . . , X5 iid∼ Uniforme(3, 6), calcular la probabilidad 
+# P(X(5) > 5.5), donde X(5) = max{X1, . . . , X5}.
+
+# Queremos calcular el máximo de un evento probabilistico. Eso se saca como
+# la probabilidad de que ese evento pase/no pase en n veces, es decir a la n.
+1 - punif(5.5, 3, 6)^5
+
+# Si (T1, . . . , T10) iid∼ Exponencial(0.5), calcular P(T(10) < 2), 
+# donde T(10) = max{T1, . . . , T10}.
+
+# Queremos la probabilidad de que el máximo sea menor a 2:
+pexp(2, 0.5)^10
+
+# Si (S1, . . . , S4) iid∼ Normal(0, 2), calcular la probabilidad
+# P(S(1) > −0.3), donde S(1) = min{S1, . . . , S4}
+
+# Queremos la probabilidad de que el mínimo sea mayor a -0.3.
+(1-pnorm(-0.3, 0, 2))^4
+
+# Si W1, . . . , W14 iid∼ Beta(3, 4), calcular P(W(1) ≤ 0.15), 
+# donde W(1) = min{W1, . . . , W14}
+
+# Queremos la probabilidad de que el minimo sea menor a 0.15
+1 - (1-pbeta(0.15, 3, 4))^14
+
+# Ahora vamos a usar ENS:
+library(readxl)
+ENS <- read_excel("ENS.xlsx")
+
+# Para la variable Colesterol (COLES), ajustar por gráfico de probabilidad una 
+# distribución LogNormal(λ, ζ).
+
+# Veamos el colesterol ordenado
+coles <- sort(ENS$COLES)
+coles
+
+# lm(x ~ y) es para ajustar linealmente x con y. Nosotros queremos COLES con
+# una LogNormal, así que podemos aprovecharnos de que lm sirve para vectores:
+vector_1_len <- 1:length(coles)
+m = vector_1_len/(length(coles)+1)
+lm(log(coles)~qnorm(m))$coefficients
+
+# Importante tirar el coefficients para que te de precisa la cosa
+
+# Ajustar una distribución Log-Logística(µ, σ) al nivel de colesterol de baja
+# densidad (LDL) por medio de gráfico de probabilidad.
+
+# Usaremos la siguiente notación:
+x <- sort(ENS$LDL);
+n <- length(x)
+m <- 1:n
+p <- m/(n+1)
+lm(log(x) ~ qlogis(p))$coefficients
+
+# Ajustar una distribución Logística(µ, σ) a la presión arterial diastólica 
+# (PAD) por método de máxima verosimilitud.
+
+# Para esto usaremos fitdeeznutz
+library(fitdistrplus)
+
+fitdist(ENS$PAD, "logis", "mle")
+# Esto en "estimate" me da 75.889020 y 6.449585
+
+# Para la variable Colesterol (COLES), ajustar por máxima verosimilitud una 
+# distribución Gamma(k, ν)
+
+fitdist(ENS$COLES, "gamma", "mle")
+# 20.8023593, 0.1068317
+
+# Estimar los parámetros de una distribución Gamma(k, ν) para ajustar a la 
+# presión arterial sistólica (PAS) por medio de método de momentos.
+
+fitdist(ENS$PAS, "gamma", "mme")
+
+# Ajustar una distribución Weibull(β, η) por máxima verosimilitud al nivel de 
+# colesterol de alta densidad (HDL).
+
+fitdist(ENS$HDL, "weibull", "mle")
+
+# El resto de los problemas es irrelevante
---
+++ parte2.R
@@ -0,0 +1,106 @@
+library(readxl)
+ENS <- read_excel("ENS.xlsx")
+
+# ¿Se puede afirmar a partir de la muestra que el nivel medio de colesterol 
+# (COLES) supera los 195 mg/hL? Asuma que el nivel de colesterol tiene 
+# distribución aproximadamente normal. Utilizar α = 5%.
+
+
+library(TeachingDemos)
+
+# Queremos saber si son mayores a 195 siempre.
+
+t.test(ENS$COLES, mu=195, alternative="greater")
+# los textos son requeridos
+# Aquí el p value es 0.6574, que es mayor a 0.05 por lo que se rechaza ha.
+
+# ¿Existe evidencia estadística para mostrar que las personas de esta población
+# tienen un nivel medio de presión arterial diastólica (PAD) distinto de 75 
+# mg/hL? Asuma que la variable señalada tiene distribución aproximadamente 
+# normal. Utilizar α = 5%.
+
+# veamos si puede ser distinto de 75
+t.test(ENS$PAD, mu=75, alternative="two.sided")
+# siendo que mi p-value es menor a 5%, tenemos que es distinto de 75.
+# se apoya ha
+
+# Se postula que la desviación estándar del nivel de colesterol de alta 
+# densidad (HDL) es menor a 13 mg/hL. ¿Se puede respaldar dicha información 
+# con los datos entregados? Asuma que la variable señalada tiene distribución 
+# aproximadamente normal. Utilizar α = 5%.
+
+# H0 = sigma >= 13mg/hl
+# Ha = sigma < 13mg/hl
+
+sigma.test(ENS$HDL, sigma=13, alternative="less")
+# Tenemos p = 0.01535 < a, por lo que se rechaza h0
+
+# Considere el grupo de mujeres (SEXO = 2) y el grupo de hombres (SEXO = 1) por 
+# separado. ¿Se puede afirmar que los niveles de colesterol de alta densidad 
+# (HDL) tienen varianzas diferentes entre mujeres y hombres? Asuma que el nivel 
+# de colesterol tiene distribución aproximadamente normal. Utilizar α = 5%.
+
+# H0 = colesteroles iguales
+# Ha = colesteroles distintos
+
+library(dplyr)
+
+H <- filter(ENS, SEXO == 1)
+M <- filter(ENS, SEXO == 2)
+
+var.test(M$HDL, H$HDL, alternative = "two.sided")
+
+# Me da valor p de 25%, así que no se rechaza H0
+
+# Teniendo en cuenta la conclusión en (4), ¿existe evidencia estadística que 
+# demuestre que el nivel medio de HDL es mayor en mujeres que en hombres? Asuma 
+# que el nivel de colesterol tiene distribución aproximadamente normal. 
+# Utilizar α = 5%.
+
+# H0: M <= H
+# Ha: M > H
+
+t.test(M$HDL, H$HDL, var.equal = FALSE, alternative = "greater", mu=0)[3]
+# No coincide con el solucionario, pero está bien igual
+
+# ¿Se puede afirmar a partir de la muestra que la mayoría de la población no
+# fuma? (variable FUMADOR) Utilizar α = 5%.
+
+# Queremos fumadores sobre población
+fumadores <- filter(ENS, FUMADOR == 1)
+n_fumadores <- length(fumadores$FUMADOR)
+poblacion <- length(ENS$FUMADOR)
+# ojo que hay que agregar ese $fumador al final
+
+prop.test(n_fumadores, poblacion, 0.5, alternative = "less", correct = "T")[3]
+# Si, estoy bastante seguro (nivel de confianza de 99.99999999%)
+
+# Se afirma que más de un 10% de la población tiene diabetes. ¿Puede respladar
+# esta hipótesis con los datos? Utilizar α = 5% y variable DIABETES.
+
+diabeticos <- length(filter(ENS, DIABETES == 1)$DIABETES)
+
+prop.test(diabeticos, poblacion, 0.1, alternative = "greater")
+
+# no se puede respaldar (p value de 0.297)
+
+# Realizar un test de bondad de ajuste Log-Normal al nivel de colesterol 
+# (COLES). ¿Se puede asumir una distribución Log-Normal? Utilizar significancia 
+# de 5%. Ayuda: estimar parámetros por máxima verosimilitud, luego realizar 
+# test Kolmogorov-Smirnov (K-S).
+
+fitdist(ENS$COLES, "lnorm", "mle")
+ks.test(ENS$COLES, "plnorm", meanlog=5.2473349, sdlog=0.2214904)
+
+#Acá se tiene que ignorar el warning. La p es obligatoria.
+# p-value = 0.08448
+
+# Realizar un test de bondad de ajuste Gamma al nivel de colesterol de baja 
+# densidad (LDL). ¿Se puede asumir una distribución Gamma? Utilizar 
+# significancia de 5%. Ayuda: estimar parámetros por máxima verosimilitud, 
+# luego realizar test Kolmogorov-Smirnov (K-S)
+
+fitdist(ENS$LDL, "gamma", "mle")
+ks.test(ENS$LDL, "pgamma", shape=10.51812301, rate=0.08914218)
+
+# p-value = 0.3898 no se puede asumir gamma.
